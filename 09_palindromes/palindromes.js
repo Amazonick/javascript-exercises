88f7d9eda4f90db5a1762bfdf2fe8696b78aa9d7
Amazonick <pzs@live.com>
2022-04-08 16:27:30 +0200

    final

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const onlyLetters = string.toLowerCase().match(/[a-z0-9]/g);
+    return onlyLetters.join('') === onlyLetters.reverse().join('');
 };
 
 // Do not edit below this line
